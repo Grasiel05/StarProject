1717be7c4797fae1f24b42273b57e262fd38e10a
Grasiel <grasielaguilar5@gmail.com>
2024-11-24 19:37:58 -0500

    configuracion de sqlite3

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+mkdir -p /app/db
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
